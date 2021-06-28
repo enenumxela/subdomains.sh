13dcb88f600ae0ead5b1347c04fd8405643fcb96
Alex Munene <62714471+enenumxela@users.noreply.github.com>
2021-06-28 18:55:30 +0300

    chore: -

---
+++ install.sh
@@ -0,0 +1,85 @@
+#!/usr/bin/env bash
+
+bold="\e[1m"
+red="\e[31m"
+cyan="\e[36m"
+blue="\e[34m"
+reset="\e[0m"
+green="\e[32m"
+yellow="\e[33m"
+underline="\e[4m"
+
+tools=(
+    tee
+    wget
+)
+missing_tools=()
+
+for tool in "${tools[@]}"
+do
+    if [ ! -x "$(command -v ${tool})" ]
+    then 
+        missing_tools+=(${tool})
+    fi
+done
+
+if [ ${#missing_tools[@]} -gt 0 ]
+then
+    sudo apt -qq -y install ${missing_tools[@]}
+fi
+
+# golang
+
+if [ ! -x "$(command -v go)" ]
+then
+    version=1.15.7
+
+    wget https://golang.org/dl/go${version}.linux-amd64.tar.gz -O /tmp/go${version}.linux-amd64.tar.gz
+
+    sudo tar -xzf /tmp/go${version}.linux-amd64.tar.gz -C /usr/local
+
+    (grep -q "export PATH=\$PATH:/usr/local/go/bin" ~/.profile) || {
+        export PATH=$PATH:/usr/local/go/bin
+        echo "export PATH=\$PATH:/usr/local/go/bin" >> ~/.profile
+        source ~/.profile
+    }
+fi
+
+# amass
+
+GO111MODULE=on go get github.com/OWASP/Amass/v3/...
+
+# subfinder
+
+GO111MODULE=on go get -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder
+
+# sigsubfind3r
+
+GO111MODULE=on go get -v github.com/signedsecurity/sigsubfind3r/cmd/sigsubfind3r
+
+# findomain
+
+if [ ! -f ${HOME}/.local/bin/findomain ]
+then
+    curl -sL https://github.com/Edu4rdSHL/findomain/releases/latest/download/findomain-linux -o ${HOME}/.local/bin/findomain
+    chmod u+x ${HOME}/.local/bin/findomain
+fi
+
+script_directory="${HOME}/.local/bin"
+
+if [ ! -d ${script_directory} ]
+then
+    mkdir -p ${script_directory}
+fi
+
+script_path="${script_directory}/${script_filename}"
+
+if [ -e "${script_path}" ]
+then
+    rm ${script_path}
+fi
+
+# subdomains.sh
+
+curl -sL https://github.com/enenumxela/subdomains.sh/raw/main/subdomains.sh -o ${script_path}
+chmod u+x ${script_path}
--- subdomains.sh
+++ subdomains.sh
@@ -46,80 +46,6 @@ display_usage() {
 EOF
 }
 
-# Function to install and setup required tools
-setup_requirements() {
-	tools=(
-		tee
-		wget
-	)
-	missing_tools=()
-
-	for tool in "${tools[@]}"
-	do
-		if [ ! -x "$(command -v ${tool})" ]
-		then 
-			missing_tools+=(${tool})
-		fi
-	done
-
-	if [ ${#missing_tools[@]} -gt 0 ]
-	then
-		if [ "${UID}" -gt 0 ]
-		then
-			echo ${password} | sudo -S apt -qq -y install ${missing_tools[@]}
-		else
-			apt -qq -y install ${missing_tools[@]}
-		fi
-	fi
-
-	if [ ! -x "$(command -v go)" ]
-	then
-		version=1.15.7
-
-		wget https://golang.org/dl/go${version}.linux-amd64.tar.gz -O /tmp/go${version}.linux-amd64.tar.gz
-
-		if [ "${UID}" -gt 0 ]
-		then
-			echo ${password} | sudo -S tar -xzf /tmp/go${version}.linux-amd64.tar.gz -C /usr/local
-		else
-			tar -xzf /tmp/go${version}.linux-amd64.tar.gz -C /usr/local
-		fi
-
-		(grep -q "export PATH=\$PATH:/usr/local/go/bin" ~/.profile) || {
-			export PATH=$PATH:/usr/local/go/bin
-			echo "export PATH=\$PATH:/usr/local/go/bin" >> ~/.profile
-			source ~/.profile
-		}
-	fi
-
-	GO111MODULE=on go get github.com/OWASP/Amass/v3/...
-	GO111MODULE=on go get -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder
-	GO111MODULE=on go get -v github.com/signedsecurity/sigsubfind3r/cmd/sigsubfind3r
-
-	if [ ! -f ${HOME}/.local/bin/findomain ]
-	then
-		curl -sL https://github.com/Edu4rdSHL/findomain/releases/latest/download/findomain-linux -o ${HOME}/.local/bin/findomain
-		chmod u+x ${HOME}/.local/bin/findomain
-	fi
-
-	script_directory="${HOME}/.local/bin"
-
-	if [ ! -d ${script_directory} ]
-	then
-		mkdir -p ${script_directory}
-	fi
-
-	script_path="${script_directory}/${script_filename}"
-
-	if [ -e "${script_path}" ]
-	then
-		rm ${script_path}
-	fi
-
-	curl -sL https://github.com/enenumxela/subdomains.sh/raw/main/subdomains.sh -o ${script_path}
-	chmod u+x ${script_path}
-}
-
 _amass() {
 	local amass_output="${output_directory}/${domain}-temp-amass-subdomains.txt"
 
@@ -250,7 +176,7 @@ do
 			keep=True
 		;;
 		--setup)
-			setup_requirements
+			curl -sL https://github.com/enenumxela/subdomains.sh/raw/main/subdomains.sh | bash -
 			exit 0
 		;;
 		-h | --help)
